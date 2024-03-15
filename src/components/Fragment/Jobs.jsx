90ef2cbb5887e27ece0000dc08b383b1eb046a5a
Hassanjadi <hassanjadi25@gmail.com>
2024-03-15 16:38:43 +0700

    feat: jobs section

---
+++ src/components/Fragment/Jobs.jsx
@@ -0,0 +1,209 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Jobs = () => {
+  return (
+    <section className="w-full bg-[#F3F3F3] py-16">
+      <div className="container mx-auto px-4 md:px-8 lg:px-16">
+        <div className="flex flex-col gap-6 items-center mb-10">
+          <h1 className="text-5xl font-bold text-center">
+            Discover the best jobs
+          </h1>
+          <p className="text-base text-center font-medium lg:w-1/2">
+            Start your career with the best companies in the world. We ensure
+            you get the best job possible
+          </p>
+        </div>
+
+        {/* Card */}
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div class="bg-white px-4 py-8 rounded-xl">
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-6">
+                <div className="flex justify-center items-center bg-gray-100 min-w-[99px] p-4 rounded-lg">
+                  <img
+                    src="https://i.ibb.co/qkr28BP/Google.png"
+                    alt="Google"
+                    className="w-full h-full bg-cover"
+                  />
+                </div>
+                <div className="flex flex-col gap-4">
+                  <p className="text-base font-bold">
+                    Quality Assurance Tester Manual Intern
+                  </p>
+                  <div className="flex gap-2 text-sm font-semibold text-gray-400">
+                    <p>Google</p>&bull;
+                    <p>Jakarta Pusat</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-6">
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Fulltime
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Onsite
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Internship
+                </p>
+              </div>
+              <p className="text-xs">
+                Google LLC is an American multinational technology company that
+                focuses on search engin...
+              </p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-400 text-xs font-semibold">
+                  2 Days ago
+                </p>
+                <span>&bull;</span>
+                <p className="text-xs font-semibold">100k - 200k/Month</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white p-4 py-8 rounded-xl">
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-6">
+                <div className="flex justify-center items-center bg-gray-100 min-w-[99px] p-4 rounded-lg">
+                  <img
+                    src="https://i.ibb.co/qkr28BP/Google.png"
+                    alt="Google"
+                    className="w-full h-full bg-cover"
+                  />
+                </div>
+                <div className="flex flex-col gap-4">
+                  <p className="text-base font-bold">
+                    Quality Assurance Tester Manual Intern
+                  </p>
+                  <div className="flex gap-2 text-sm font-semibold text-gray-400">
+                    <p>Google</p>&bull;
+                    <p>Jakarta Pusat</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-6">
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Fulltime
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Onsite
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Internship
+                </p>
+              </div>
+              <p className="text-xs">
+                Google LLC is an American multinational technology company that
+                focuses on search engin...
+              </p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-400 text-xs font-semibold">
+                  2 Days ago
+                </p>
+                <span>&bull;</span>
+                <p className="text-xs font-semibold">100k - 200k/Month</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white p-4 py-8 rounded-xl">
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-6">
+                <div className="flex justify-center items-center bg-gray-100 min-w-[99px] p-4 rounded-lg">
+                  <img
+                    src="https://i.ibb.co/qkr28BP/Google.png"
+                    alt="Google"
+                    className="w-full h-full bg-cover"
+                  />
+                </div>
+                <div className="flex flex-col gap-4">
+                  <p className="text-base font-bold">
+                    Quality Assurance Tester Manual Intern
+                  </p>
+                  <div className="flex gap-2 text-sm font-semibold text-gray-400">
+                    <p>Google</p>&bull;
+                    <p>Jakarta Pusat</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-6">
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Fulltime
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Onsite
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Internship
+                </p>
+              </div>
+              <p className="text-xs">
+                Google LLC is an American multinational technology company that
+                focuses on search engin...
+              </p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-400 text-xs font-semibold">
+                  2 Days ago
+                </p>
+                <span>&bull;</span>
+                <p className="text-xs font-semibold">100k - 200k/Month</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white p-4 py-8 rounded-xl">
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-6">
+                <div className="flex justify-center items-center bg-gray-100 min-w-[99px] p-4 rounded-lg">
+                  <img
+                    src="https://i.ibb.co/qkr28BP/Google.png"
+                    alt="Google"
+                    className="w-full h-full bg-cover"
+                  />
+                </div>
+                <div className="flex flex-col gap-4">
+                  <p className="text-base font-bold">
+                    Quality Assurance Tester Manual Intern
+                  </p>
+                  <div className="flex gap-2 text-sm font-semibold text-gray-400">
+                    <p>Google</p>&bull;
+                    <p>Jakarta Pusat</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-6">
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Fulltime
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Onsite
+                </p>
+                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
+                  Internship
+                </p>
+              </div>
+              <p className="text-xs">
+                Google LLC is an American multinational technology company that
+                focuses on search engin...
+              </p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-400 text-xs font-semibold">
+                  2 Days ago
+                </p>
+                <span>&bull;</span>
+                <p className="text-xs font-semibold">100k - 200k/Month</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* View All Job */}
+        <div className="flex justify-center items-center gap-1 pt-6">
+          <p className="flex gap-2">Did not find what you’re looking for?</p>
+          {""}
+          <Link to="/vacancy" className="text-indigo-500">
+            View all jobs
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
