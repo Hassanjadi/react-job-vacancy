edef963a06052f8df9f7c37fd058deca7aa74915
Hassanjadi <hassanjadi25@gmail.com>
2024-03-15 16:35:35 +0700

    fix: container and Link

--- src/components/Fragment/FooterClient.jsx
+++ src/components/Fragment/FooterClient.jsx
@@ -1,37 +1,39 @@
 import React from "react";
-import FeatherIcon from "feather-icons-react";
 import { Link } from "react-router-dom";
+import FeatherIcon from "feather-icons-react";
 
 export const FooterClient = () => {
   return (
-    <div class="w-full px-4 lg:px-24 py-5 items-center bg-[#F9F9F9]">
+    <div class="w-full py-5 bg-[#F9F9F9]">
       <hr className="mb-6" />
-      <div class="flex flex-col justify-between items-center mx-auto lg:flex-row lg:container">
-        <p class="text-base font-medium text-[#525E76] mb-4 lg:mb-0">
-          &#169;2023 LokerIn by Hassanjadi
-        </p>
-        <div class="flex flex-col items-center gap-4 lg:flex-row lg:gap-8">
-          <div class="flex  gap-2 lg:flex-row lg:gap-4">
-            <a href="#" class="text-base font-medium text-[#525E76]">
-              Terms
-            </a>
-            <a href="#" class="text-base font-medium text-[#525E76]">
-              Privacy
-            </a>
-            <a href="#" class="text-base font-medium text-[#525E76]">
-              Legal
-            </a>
-          </div>
-          <div class="flex flex-row justify-center gap-4">
-            <a href="#" class="text-[#525E76]">
-              <FeatherIcon icon="github" fill="#525E76" stroke="#525E76" />
-            </a>
-            <a href="#" class="text-[#525E76]">
-              <FeatherIcon icon="linkedin" fill="#525E76" stroke="#525E76" />
-            </a>
-            <a href="#" class="text-[#525E76]">
-              <FeatherIcon icon="twitter" fill="#525E76" stroke="#525E76" />
-            </a>
+      <div class="container mx-auto px-4 md:px-8 lg:px-16 ">
+        <div className="flex flex-col justify-between items-center lg:flex-row">
+          <p class="text-base font-medium text-[#525E76] mb-4 lg:mb-0">
+            &#169;2023 LokerIn by Hassanjadi
+          </p>
+          <div class="flex flex-col items-center gap-4 lg:flex-row lg:gap-8">
+            <div class="flex  gap-2 lg:flex-row lg:gap-4">
+              <Link to="#" class="text-base font-medium text-[#525E76]">
+                Terms
+              </Link>
+              <Link to="#" class="text-base font-medium text-[#525E76]">
+                Privacy
+              </Link>
+              <Link to="#" class="text-base font-medium text-[#525E76]">
+                Legal
+              </Link>
+            </div>
+            <div class="flex flex-row justify-center gap-4">
+              <Link to="#" class="text-[#525E76]">
+                <FeatherIcon icon="github" fill="#525E76" stroke="#525E76" />
+              </Link>
+              <Link to="#" class="text-[#525E76]">
+                <FeatherIcon icon="linkedin" fill="#525E76" stroke="#525E76" />
+              </Link>
+              <Link to="#" class="text-[#525E76]">
+                <FeatherIcon icon="twitter" fill="#525E76" stroke="#525E76" />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
