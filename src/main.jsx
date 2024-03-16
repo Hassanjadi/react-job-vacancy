d8750ff72ea347aaa369c29b2921f609c2465568
Hassanjadi <hassanjadi25@gmail.com>
2024-03-16 16:16:33 +0700

    feat: job vacancy router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import "./index.css";
 import React from "react";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import ReactDOM from "react-dom/client";
+import { Vacancy } from "./pages/Vacancy";
 import { ErrorPage } from "./pages/404.jsx";
-import { LoginPage } from "./pages/login.jsx";
-import { RegisterPage } from "./pages/register.jsx";
+import { LoginPage } from "./pages/Login.jsx";
+import { RegisterPage } from "./pages/Register.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
+  {
+    path: "/job-vacancy",
+    element: <Vacancy />,
+  },
   {
     path: "/",
     element: <Home />,
