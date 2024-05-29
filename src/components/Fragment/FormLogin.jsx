ea8fe6d478583885c5f4cf2703e6a3e6d03ab869
Hassanjadi <hassanjadi25@gmail.com>
2024-05-29 16:16:30 +0700

    feat: endpoint login

--- src/components/Fragment/FormLogin.jsx
+++ src/components/Fragment/FormLogin.jsx
@@ -1,20 +1,42 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../Element/Button";
 import { InputForm } from "../Element/Input";
+import { login } from "../../services/auth.services";
 
 export const FormLogin = () => {
-  const emailRef = useRef(null);
+  const [loginFailed, setLoginFailed] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    const data = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem("token", res);
+        window.location.href = "/dashboard";
+      } else {
+        setLoginFailed(res.response.data);
+      }
+    });
+  };
+
+  const usernameRef = useRef(null);
   useEffect(() => {
-    emailRef.current.focus();
+    usernameRef.current.focus();
   }, []);
+
   return (
-    <form className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5" onSubmit={handleLogin}>
       <InputForm
-        label="Email"
-        type="email"
-        name="email"
-        placeholder="example@gmail.com"
-        ref={emailRef}
+        label="Username"
+        type="text"
+        name="username"
+        placeholder="Username"
+        ref={usernameRef}
       />
       <InputForm
         label="Password"
@@ -22,7 +44,12 @@ export const FormLogin = () => {
         name="password"
         placeholder="6+ password"
       />
-      <Button classname="bg-[#635BFF] w-full">Login</Button>
+      <Button classname="bg-[#635BFF] w-full" type="submit">
+        Login
+      </Button>
+      {loginFailed && (
+        <p className="text-red-500 text-center text-sm">{loginFailed}</p>
+      )}
     </form>
   );
 };
