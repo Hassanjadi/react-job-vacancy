1ac91b9eee0dc22d70e393eef35dc0981f3cd4e5
Hassanjadi <hassanjadi25@gmail.com>
2024-05-30 22:07:10 +0700

    fix: endpoint auth

--- src/components/Fragment/FormLogin.jsx
+++ src/components/Fragment/FormLogin.jsx
@@ -1,55 +1,78 @@
-import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 import { Button } from "../Element/Button";
 import { InputForm } from "../Element/Input";
-import { login } from "../../services/auth.services";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 export const FormLogin = () => {
-  const [loginFailed, setLoginFailed] = useState("");
+  let navigate = useNavigate();
+
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+
+    setInput({ ...input, [name]: value });
+  };
 
   const handleLogin = (event) => {
     event.preventDefault();
 
-    const data = {
-      username: event.target.username.value,
-      password: event.target.password.value,
-    };
+    const { email, password } = input;
 
-    login(data, (status, res) => {
-      if (status) {
-        localStorage.setItem("token", res);
-        window.location.href = "/dashboard";
-      } else {
-        setLoginFailed(res.response.data);
-      }
-    });
+    axios
+      .post("https://dev-example.sanbercloud.com/api/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        const data = res.data;
+        Cookies.set("token", data.token, { expires: 1 });
+        navigate("/");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
-  const usernameRef = useRef(null);
+  const emailRef = useRef(null);
   useEffect(() => {
-    usernameRef.current.focus();
+    emailRef.current.focus();
   }, []);
 
   return (
     <form className="flex flex-col gap-5" onSubmit={handleLogin}>
       <InputForm
-        label="Username"
-        type="text"
-        name="username"
-        placeholder="Username"
-        ref={usernameRef}
+        label="Email"
+        type="email"
+        name="email"
+        placeholder="example@gmail.com"
+        ref={emailRef}
+        value={input.email}
+        onChange={handleInput}
+        required
       />
       <InputForm
         label="Password"
         type="password"
         name="password"
-        placeholder="6+ password"
+        placeholder="8+ password"
+        value={input.password}
+        onChange={handleInput}
+        required
+        minLength="8"
       />
       <Button classname="bg-[#635BFF] w-full" type="submit">
         Login
       </Button>
-      {loginFailed && (
+      {/* {loginFailed && (
         <p className="text-red-500 text-center text-sm">{loginFailed}</p>
-      )}
+      )} */}
     </form>
   );
 };
