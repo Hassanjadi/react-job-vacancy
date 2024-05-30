648178357c1d1751b8642d29d2cbe92b3796c929
Hassanjadi <hassanjadi25@gmail.com>
2024-05-30 10:45:54 +0700

    fix: card style

--- src/components/Fragment/CardJobs.jsx
+++ src/components/Fragment/CardJobs.jsx
@@ -15,41 +15,41 @@ export const CardJobs = () => {
       {jobs.length > 0 &&
         jobs.map((job) => (
           <div className="bg-white px-4 py-8 rounded-xl" key={job.id}>
-            <div className="flex flex-col gap-6">
-              <div className="flex items-center gap-6">
-                <div className="flex justify-center items-center bg-gray-100 min-w-[99px] p-4 rounded-lg">
-                  <img
-                    src={job.company_image_url}
-                    alt={job.company_name}
-                    className="w-full h-full bg-cover"
-                  />
-                </div>
-                <div className="flex flex-col gap-4">
-                  <p className="text-base font-bold">{job.title}</p>
-                  <div className="flex gap-2 text-sm font-semibold text-gray-400">
-                    <p>{job.company_name}</p>&bull;
-                    <p>{job.company_city}</p>
+            <div className="flex flex-col h-[350px] justify-between">
+              <div className="flex flex-col gap-4">
+                <div className="flex items-center gap-6">
+                  <div className="flex justify-center items-center bg-gray-100 max-w-[99px] min-h-[99px] p-4 rounded-lg">
+                    <img
+                      src={job.company_image_url}
+                      alt={job.company_name}
+                      className="w-full h-full bg-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <p className="text-base font-bold">{job.title}</p>
+                    <div className="flex gap-2 text-sm font-semibold text-gray-400">
+                      <p>{job.company_name}</p>&bull;
+                      <p>{job.company_city}</p>
+                    </div>
                   </div>
                 </div>
+                <div className="flex gap-2 text-sm">
+                  <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
+                    {job.job_tenure}
+                  </p>
+                  <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
+                    {job.job_type}
+                  </p>
+                  <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
+                    {job.job_status}
+                  </p>
+                </div>
+                <p className="text-xs">{job.job_description}</p>
               </div>
-              <div className="flex gap-6">
-                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
-                  {job.job_tenure}
-                </p>
-                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
-                  {job.job_type}
-                </p>
-                <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600 text-sm">
-                  {job.job_status}
-                </p>
-              </div>
-              <p className="text-xs">{job.job_description}</p>
-              <div className="flex items-center gap-2">
-                <p className="text-gray-400 text-xs font-semibold">
-                  {job.updated_at}
-                </p>
+              <div className="flex items-center gap-2 text-xs">
+                <p className="text-gray-400 font-semibold">{job.updated_at}</p>
                 <span>&bull;</span>
-                <p className="text-xs font-semibold">
+                <p className="font-semibold">
                   {job.salary_min} - {job.salary_max}/Month
                 </p>
               </div>
