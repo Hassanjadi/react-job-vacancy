fb2174ce7d8db2704386e20c600aebe5dc8fe78b
Hassanjadi <hassanjadi25@gmail.com>
2024-05-30 22:07:39 +0700

    refactor

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,28 +1,19 @@
+import React from "react";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 import { Button } from "../components/Element/Button";
-import { getUsername } from "../services/auth.services";
-import React, { useEffect, useState } from "react";
 
 export const Dashboard = () => {
-  const [username, setUsername] = useState();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      window.location.href = "/login";
-    } else {
-      setUsername(getUsername(token));
-    }
-  }, []);
-
+  const navigate = useNavigate();
   const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login";
+    Cookies.remove("token");
+    navigate("/login");
   };
 
   return (
     <div className="max-w-screen h-screen flex flex-col justify-center items-center">
       <h1 className="font-bold text-xl text-center mb-5">
-        Hello {username} <br /> dashboard is ready coming soon 🎈
+        dashboard is ready coming soon 🎈
       </h1>
       <Button onClick={handleLogout}>Logout</Button>
     </div>
