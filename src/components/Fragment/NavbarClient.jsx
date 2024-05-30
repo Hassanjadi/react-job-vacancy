0465df1fd9a64106e568fd9923c12c10454a15aa
Hassanjadi <hassanjadi25@gmail.com>
2024-05-30 22:09:01 +0700

    fix: navlink dashboard

--- src/components/Fragment/NavbarClient.jsx
+++ src/components/Fragment/NavbarClient.jsx
@@ -1,8 +1,10 @@
+import Cookies from "js-cookie";
 import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
 import Logo from "../../assets/images/Logo.png";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavbarClient = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -11,6 +13,10 @@ export const NavbarClient = () => {
 
   const closeMenu = () => {
     setIsMenuOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
@@ -27,12 +33,14 @@ export const NavbarClient = () => {
           >
             Job Vacancy
           </NavLink>
-          <NavLink
-            to="/dashboard"
-            className="font-normal text-lg hover:font-semibold"
-          >
-            Dashboard
-          </NavLink>
+          {Cookies.get("token") && (
+            <NavLink
+              to="/dashboard"
+              className="font-normal text-lg hover:font-semibold"
+            >
+              Dashboard
+            </NavLink>
+          )}
         </div>
         <button className="lg:hidden focus:outline-none" onClick={toggleMenu}>
           <svg
@@ -87,12 +95,15 @@ export const NavbarClient = () => {
               >
                 Job Vacancy
               </NavLink>
-              <NavLink
-                to="#"
-                className="font-normal text-lg hover:font-semibold"
-              >
-                Dashboard
-              </NavLink>
+              {Cookies.get("token") && (
+                <NavLink
+                  to="#"
+                  className="font-normal text-lg hover:font-semibold"
+                >
+                  Dashboard
+                </NavLink>
+              )}
+
               <Link
                 to="/login"
                 className="h-10 px-6 font-medium text-base rounded-md text-white bg-[#635BFF] flex items-center"
@@ -103,12 +114,47 @@ export const NavbarClient = () => {
           </div>
         )}
         <div className="hidden lg:flex gap-4 items-center">
-          <Link
-            to="/login"
-            className="h-10 px-6 font-medium text-base rounded-md text-white bg-[#635BFF] flex items-center"
-          >
-            Sign In
-          </Link>
+          {!Cookies.get("token") && (
+            <Link
+              to="/login"
+              className="h-10 px-6 font-medium text-base rounded-md text-white bg-[#635BFF] flex items-center"
+            >
+              Sign In
+            </Link>
+          )}
+          {Cookies.get("token") && (
+            <div className="flex items-center">
+              <div className="relative">
+                <button
+                  onClick={toggleDropdown}
+                  className="flex items-center text-white focus:outline-none"
+                >
+                  <img
+                    src="https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.webp"
+                    alt="Avatar"
+                    className="h-10 w-10 rounded-full"
+                  />
+                </button>
+                {isDropdownOpen && (
+                  <div className="absolute top-10 right-0 bg-white border rounded shadow">
+                    <ul className="py-2">
+                      <li>
+                        <Link
+                          onClick={() => {
+                            Cookies.remove("token");
+                            navigate("/");
+                          }}
+                          className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
+                        >
+                          Logout
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
