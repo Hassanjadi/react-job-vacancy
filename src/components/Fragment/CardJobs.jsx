c98ec88d6713b976af7e6d1c3485d5f7f11c9ce2
Hassanjadi <hassanjadi25@gmail.com>
2024-05-31 13:44:09 +0700

    feat: add link to detail job page

--- src/components/Fragment/CardJobs.jsx
+++ src/components/Fragment/CardJobs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getJobs } from "../../services/jobs.services";
+import { Link } from "react-router-dom";
 
 export const CardJobs = () => {
   const [jobs, setJobs] = useState([]);
@@ -47,48 +48,50 @@ export const CardJobs = () => {
       {jobs.length > 0 &&
         jobs.map((job) => (
           <div className="bg-white px-4 py-4 rounded-xl" key={job.id}>
-            <div className="flex flex-col h-[350px] justify-between">
-              <div className="flex flex-col gap-4">
-                <div className="flex items-center gap-6">
-                  <div className="flex justify-center items-center bg-gray-100 max-w-[99px] min-h-[99px] p-4 rounded-lg">
-                    <img
-                      src={job.company_image_url}
-                      alt={job.company_name}
-                      className="w-full h-full bg-cover"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p className="text-base font-bold">{job.title}</p>
-                    <div className="flex gap-2 text-sm font-semibold text-gray-400">
-                      <p>{job.company_name}</p>&bull;
-                      <p>{job.company_city}</p>
+            <Link to={`/job/${job.id}`}>
+              <div className="flex flex-col h-[350px] justify-between">
+                <div className="flex flex-col gap-4">
+                  <div className="flex items-center gap-6">
+                    <div className="flex justify-center items-center bg-gray-100 max-w-[99px] min-h-[99px] p-4 rounded-lg">
+                      <img
+                        src={job.company_image_url}
+                        alt={job.company_name}
+                        className="w-full h-full bg-cover"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <p className="text-base font-bold">{job.title}</p>
+                      <div className="flex gap-2 text-sm font-semibold text-gray-400">
+                        <p>{job.company_name}</p>&bull;
+                        <p>{job.company_city}</p>
+                      </div>
                     </div>
                   </div>
+                  <div className="flex gap-2 text-sm">
+                    <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
+                      {job.job_tenure}
+                    </p>
+                    <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
+                      {job.job_type}
+                    </p>
+                    <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
+                      {job.job_status === 0 ? "Close" : "Open"}
+                    </p>
+                  </div>
+                  <p className="text-xs">{job.job_description}</p>
                 </div>
-                <div className="flex gap-2 text-sm">
-                  <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
-                    {job.job_tenure}
+                <div className="flex items-center gap-4 text-sm">
+                  <p className="text-gray-400 font-semibold">
+                    {handleTime(job.updated_at)}
                   </p>
-                  <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
-                    {job.job_type}
-                  </p>
-                  <p className="px-2 py-1 bg-blue-100 rounded-md text-blue-600">
-                    {job.job_status === 0 ? "Close" : "Open"}
+                  <span>&bull;</span>
+                  <p className="font-semibold text-sm">
+                    {handleSalary(job.salary_min)} -{" "}
+                    {handleSalary(job.salary_max)}/Month
                   </p>
                 </div>
-                <p className="text-xs">{job.job_description}</p>
               </div>
-              <div className="flex items-center gap-4 text-sm">
-                <p className="text-gray-400 font-semibold">
-                  {handleTime(job.updated_at)}
-                </p>
-                <span>&bull;</span>
-                <p className="font-semibold text-sm">
-                  {handleSalary(job.salary_min)} -{" "}
-                  {handleSalary(job.salary_max)}/Month
-                </p>
-              </div>
-            </div>
+            </Link>
           </div>
         ))}
     </div>
