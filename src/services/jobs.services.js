5fb1bddac68cba0ef776e12af5a77d807f89e432
Hassanjadi <hassanjadi25@gmail.com>
2024-05-31 13:38:28 +0700

    feat: get detail job

--- src/services/jobs.services.js
+++ src/services/jobs.services.js
@@ -10,3 +10,14 @@ export const getJobs = (callback) => {
       console.log(err);
     });
 };
+
+export const getDetailJob = (id, callback) => {
+  axios
+    .get(`https://dev-example.sanbercloud.com/api/job-vacancy/${id}`)
+    .then((res) => {
+      callback(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
