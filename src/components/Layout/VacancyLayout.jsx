3135d9b8a2494d3040b327ba55fbf7486f8dd57d
Hassanjadi <hassanjadi25@gmail.com>
2024-06-01 22:29:43 +0700

    refactor

--- src/components/Layout/VacancyLayout.jsx
+++ src/components/Layout/VacancyLayout.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { CardJobs } from "../Fragment/CardJobs";
-import { HeroVacancy } from "../Fragment/HeroVacancy";
-import { FooterClient } from "../Fragment/FooterClient";
-import { NavbarClient } from "../Fragment/NavbarClient";
-import { SearchVacancy } from "../Fragment/SearchVacancy";
+import { CardJobs } from "../../components/Fragment/CardJobs";
+import { HeroVacancy } from "../../components/Fragment/HeroVacancy";
+import { SearchInput } from "../../components/Fragment/SearchInput";
+import { SalaryFilter } from "../../components/Fragment/SalaryFilter";
+import { NavbarClient } from "../../components/Fragment/NavbarClient";
+import { FooterClient } from "../../components/Fragment/FooterClient";
+import { JobTenureFilter } from "../../components/Fragment/JobTenureFilter";
+import { CompanyCityFilter } from "../../components/Fragment/CompanyCityFilter";
 
 export const VacancyLayout = () => {
   return (
     <>
       <NavbarClient />
       <HeroVacancy />
-      <SearchVacancy />
+      <section className="w-full bg-[#F9F9F9] py-16">
+        <div className="container mx-auto px-4 md:px-8 lg:px-16">
+          <div className="flex flex-col gap-4 md:flex-row md:w-full lg:w-full">
+            <div className="flex flex-col gap-4 md:flex-row md:w-4/5">
+              <CompanyCityFilter />
+              <JobTenureFilter />
+              <SalaryFilter />
+            </div>
+            <div className="lg:w-1/4">
+              <SearchInput />
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="w-full bg-[#F9F9F9] pb-16">
         <div className="container mx-auto px-4 md:px-8 lg:px-16">
           <CardJobs />
