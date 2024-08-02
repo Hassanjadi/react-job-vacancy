a800b8ee8b1057bc34d7647f03a5a54686c85f21
Hassanjadi <hassanjadi25@gmail.com>
2024-08-02 17:54:58 +0700

    fix: modification state management

--- src/context/JobContext.jsx
+++ src/context/JobContext.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
+import Cookies from "js-cookie";
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { getJobs } from "../services/jobs.services";
 
 const JobContext = createContext();
 
@@ -8,13 +9,183 @@ export const JobProvider = ({ children }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [companyCity, setCompanyCity] = useState("");
   const [jobTenure, setJobTenure] = useState("");
+  const [currentId, setCurrentId] = useState(-1);
+  const [fetchStatus, setFetchStatus] = useState(true);
   const [salaryRange, setSalaryRange] = useState([0, Infinity]);
+  const [input, setInput] = useState({
+    title: "",
+    job_description: "",
+    job_qualification: "",
+    job_type: "",
+    job_tenure: "",
+    job_status: "",
+    company_name: "",
+    company_image_url: "",
+    company_city: "",
+    salary_min: "",
+    salary_max: "",
+  });
+  const [inputEdit, setInputEdit] = useState({
+    title: "",
+    job_description: "",
+    job_qualification: "",
+    job_type: "",
+    job_tenure: "",
+    job_status: "",
+    company_name: "",
+    company_image_url: "",
+    company_city: "",
+    salary_min: "",
+    salary_max: "",
+  });
 
   useEffect(() => {
-    getJobs((data) => {
-      setJobs(data);
+    if (fetchStatus === true) {
+      axios
+        .get("https://dev-example.sanbercloud.com/api/job-vacancy")
+        .then((res) => {
+          setJobs([...res.data.data]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      setFetchStatus(false);
+    }
+  }, [fetchStatus, setFetchStatus]);
+
+  const handleInput = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    setInput({ ...input, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const {
+      title,
+      job_description,
+      job_qualification,
+      job_type,
+      job_tenure,
+      job_status,
+      company_name,
+      company_image_url,
+      company_city,
+      salary_min,
+      salary_max,
+    } = input;
+
+    if (currentId === -1) {
+      axios
+        .post(
+          "https://dev-example.sanbercloud.com/api/job-vacancy",
+          {
+            title,
+            job_description,
+            job_qualification,
+            job_type,
+            job_tenure,
+            job_status,
+            company_name,
+            company_image_url,
+            company_city,
+            salary_min,
+            salary_max,
+          },
+          { headers: { Authorization: "Bearer " + Cookies.get("token") } }
+        )
+        .then((res) => {
+          console.log(res);
+          setFetchStatus(true);
+        });
+    } else {
+      axios
+        .put(
+          ` https://dev-example.sanbercloud.com/api/job-vacancy/${currentId}`,
+          {
+            title,
+            job_description,
+            job_qualification,
+            job_type,
+            job_tenure,
+            job_status,
+            company_name,
+            company_image_url,
+            company_city,
+            salary_min,
+            salary_max,
+          },
+          { headers: { Authorization: "Bearer " + Cookies.get("token") } }
+        )
+        .then((res) => {
+          setFetchStatus(true);
+        });
+    }
+
+    setCurrentId(-1);
+
+    setInput({
+      title: "",
+      job_description: "",
+      job_qualification: "",
+      job_type: "",
+      job_tenure: "",
+      job_status: "",
+      company_name: "",
+      company_image_url: "",
+      company_city: "",
+      salary_min: "",
+      salary_max: "",
     });
-  }, []);
+  };
+
+  const handleEdit = (event) => {
+    let id = parseInt(event.target.value);
+    setCurrentId(id);
+
+    axios
+      .get(`https://dev-example.sanbercloud.com/api/job-vacancy/${id}`)
+      .then((res) => {
+        let data = res.data;
+
+        setInput({
+          title: data.title,
+          job_description: data.job_description,
+          job_qualification: data.job_qualification,
+          job_type: data.job_type,
+          job_tenure: data.job_tenure,
+          job_status: data.job_status,
+          company_name: data.company_name,
+          company_image_url: data.company_image_url,
+          company_city: data.company_city,
+          salary_min: data.salary_min,
+          salary_max: data.salary_max,
+        });
+      });
+  };
+
+  const handleDelete = (event) => {
+    let idData = parseInt(event.target.value);
+
+    if (window.confirm("Apakah Anda yakin ingin menghapus data ini?")) {
+      axios
+        .delete(
+          `https://dev-example.sanbercloud.com/api/job-vacancy/${idData}`,
+          {
+            headers: { Authorization: `Bearer ${Cookies.get("token")}` },
+          }
+        )
+        .then((res) => {
+          console.log("Data deleted successfully:", res);
+          setFetchStatus(true);
+        })
+        .catch((error) => {
+          console.error("Error deleting data:", error);
+        });
+    }
+  };
 
   const filteredJobs = jobs.filter((job) => {
     const matchesSearch = job.title
@@ -29,18 +200,71 @@ export const JobProvider = ({ children }) => {
     return matchesSearch && matchesCity && matchesTenure && matchesSalary;
   });
 
+  const handleTime = (dateTime) => {
+    const currentDate = new Date();
+    const targetDate = new Date(dateTime);
+
+    const timeDifference = currentDate - targetDate;
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (daysDifference === 0) {
+      return "Today";
+    } else if (daysDifference === 1) {
+      return "Yesterday";
+    } else {
+      return `${daysDifference} days ago`;
+    }
+  };
+
+  const handleSalary = (salary) => {
+    if (typeof salary !== "number") {
+      return "Invalid input";
+    }
+
+    const units = ["", "k", "M", "B", "T"];
+    let unitIndex = 0;
+    while (salary >= 1000 && unitIndex < units.length - 1) {
+      salary /= 1000;
+      unitIndex++;
+    }
+
+    return `${salary.toFixed(0)}${units[unitIndex]}`;
+  };
+
+  let state = {
+    filteredJobs,
+    jobs,
+    setJobs,
+    input,
+    setInput,
+    inputEdit,
+    setInputEdit,
+    searchQuery,
+    setSearchQuery,
+    companyCity,
+    setCompanyCity,
+    jobTenure,
+    setJobTenure,
+    salaryRange,
+    setSalaryRange,
+    currentId,
+    setCurrentId,
+  };
+
+  let handleFunction = {
+    handleSalary,
+    handleTime,
+    handleInput,
+    handleSubmit,
+    handleDelete,
+    handleEdit,
+  };
+
   return (
     <JobContext.Provider
       value={{
-        jobs: filteredJobs,
-        searchQuery,
-        setSearchQuery,
-        companyCity,
-        setCompanyCity,
-        jobTenure,
-        setJobTenure,
-        salaryRange,
-        setSalaryRange,
+        state,
+        handleFunction,
       }}
     >
       {children}
