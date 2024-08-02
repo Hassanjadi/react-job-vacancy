b6e490ad16df063f28ec87cf26a2b3d4e83d2e3c
Hassanjadi <hassanjadi25@gmail.com>
2024-08-02 17:52:19 +0700

    feat: router dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import "./index.css";
 import React from "react";
 import { Home } from "./pages/Home";
-import ReactDOM from "react-dom/client";
 import { ErrorPage } from "./pages/404";
-import { LoginPage } from "./pages/Login";
+import ReactDOM from "react-dom/client";
 import { Vacancy } from "./pages/Vacancy";
+import { LoginPage } from "./pages/Login";
+import { DataForm } from "./pages/DataForm";
+import { ListJobs } from "./pages/ListJobs";
 import { Dashboard } from "./pages/Dashboard";
 import { RegisterPage } from "./pages/Register";
 import { DetailJobPage } from "./pages/DetailJob";
 import { ProtectedRoute } from "./components/Utils/ProtectedRoute";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { JobProvider } from "./context/JobContext";
+import { Profile } from "./pages/Profile";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +30,30 @@ const router = createBrowserRouter([
     element: (
       <ProtectedRoute requiresAuth={true}>
         <Dashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/dashboard/list-job-vacancy",
+    element: (
+      <ProtectedRoute requiresAuth={true}>
+        <ListJobs />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/dashboard/form",
+    element: (
+      <ProtectedRoute requiresAuth={true}>
+        <DataForm />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/dashboard/profile",
+    element: (
+      <ProtectedRoute requiresAuth={true}>
+        <Profile />,
       </ProtectedRoute>
     ),
   },
