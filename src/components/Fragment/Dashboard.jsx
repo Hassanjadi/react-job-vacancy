777919b2c6a597f110b345d2350e4f7eaa22ec6c
Hassanjadi <hassanjadi25@gmail.com>
2024-08-03 13:36:54 +0700

    feat: card dashboard

--- src/components/Fragment/Dashboard.jsx
+++ src/components/Fragment/Dashboard.jsx
@@ -3,8 +3,18 @@ import React from "react";
 export const Dashboard = () => {
   return (
     <div className="p-4 sm:ml-64">
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-        Dashboard
+      <div className="p-4 rounded-lg  mt-14">
+        <a
+          href="#"
+          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">
+            5
+          </h5>
+          <p className="text-3xl font-semibold text-gray-700 dark:text-gray-400">
+            Jobs Total
+          </p>
+        </a>
       </div>
     </div>
   );
